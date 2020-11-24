fc8736acd04ba31afae659b230a6b8e099e68951
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2020-11-24 09:50:35 +0100

    Added class QueueException

---
+++ src/stack/QueueException.java
@@ -0,0 +1,12 @@
+package stack;
+
+public class QueueException extends Exception{
+    public QueueException(String message){
+        super(message);
+    }
+
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
