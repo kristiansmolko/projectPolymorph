ea349b0398432e4d6d648a0732b1e59974fb34a6
Kristian Smolko <kristian.smolko@kosickaakademia.sk>
2020-11-24 09:47:13 +0100

    Added class StackException

---
+++ src/stack/StackException.java
@@ -0,0 +1,11 @@
+package stack;
+
+public class StackException extends Exception{
+    public StackException(String message){
+        super(message);
+    }
+    @Override
+    public String getMessage() {
+        return super.getMessage();
+    }
+}
